5b633bccd4d4703585571052ce10c26d1b42b93c
1121yuy0610-droid <1121yuy0610@gmail.com>
2026-08-12 22:13:01 +0800

    Implement basic HTTP proxy server in index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const https = require('https');
+
+const UPSTREAM = "https://sullyos-ams.sullyos.workers.dev";
+
+const server = http.createServer((req, res) => {
+  const targetUrl = new URL(req.url, UPSTREAM);
+  const proxyReq = https.request(targetUrl, {
+    method: req.method,
+    headers: req.headers
+  }, (proxyRes) => {
+    res.writeHead(proxyRes.statusCode, proxyRes.headers);
+    proxyRes.pipe(res);
+  });
+  req.pipe(proxyReq);
+  proxyReq.on('error', err => {
+    res.writeHead(502);
+    res.end("proxy error");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, ()=>{});
